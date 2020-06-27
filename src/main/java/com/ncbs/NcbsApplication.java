2a9e43b251c5aa26db63b7180e96a4b9b33cfb8f
nasir007 <nasir.ali@clickchain.com>
2020-06-27 18:52:39 +0500

    Initial commit

---
+++ src/main/java/com/ncbs/NcbsApplication.java
@@ -0,0 +1,13 @@
+package com.ncbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NcbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ncbs/NcbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NcbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
